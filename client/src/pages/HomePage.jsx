4e7ad93b85d6e402176cb6aa1ec849e10b89aaf0
pula-ashok <ashokpula0307@gmail.com>
2024-11-25 18:22:11 +0530

    register ui skeleton completed

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,24 +2,7 @@ import React from 'react'
 
 const HomePage = () => {
     return (
-        <div className='register'>
-            <div className="register_content">
-                <form action="">
-                    <input type="text" placeholder='First Name' name='firstName' required />
-                    <input type="text" placeholder='Last Name' name='lastName' required />
-                    <input type="email" placeholder='Email' name='email' required />
-                    <input type="password" placeholder='Password' name='password' required />
-                    <input type="password" placeholder='Confirm Password' name='confirmPassword' required />
-                    <input type="file" name='profileImage' accept='image/*' hidden id='image' />
-                    <label htmlFor="image">
-                        <img src='../../public/assets/addImage.png' alt='profile image' />
-                        <p>Upload Profile Photo</p>
-                    </label>
-                    <button type='submit'>REGISTER</button>
-                </form>
-                <a href="/login">Already hava an account? Log In Here</a>
-            </div>
-        </div>
+        <div>HomePage</div>
     )
 }
 
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import '../styles/Register.scss'
 
 const RegisterPage = () => {
     return (
-        <div>RegisterPage</div>
+        <div className='register'>
+            <div className="register_content">
+                <form action="" className='register_content_form'>
+                    <input type="text" placeholder='First Name' name='firstName' required autoComplete={false} />
+                    <input type="text" placeholder='Last Name' name='lastName' required autoComplete={false} />
+                    <input type="email" placeholder='Email' name='email' required autoComplete={false} />
+                    <input type="password" placeholder='Password' name='password' />
+                    <input type="password" placeholder='Confirm Password' name='confirmPassword' />
+                    <input type="file" name='profileImage' accept='image/*' hidden id='image' />
+                    <label htmlFor="image">
+                        <img src='../../public/assets/addImage.png' alt='profile image' />
+                        <p>Upload Profile Photo</p>
+                    </label>
+                    <button type='submit'>REGISTER</button>
+                </form>
+                <a href="/login">Already hava an account? Log In Here</a>
+            </div>
+        </div>
     )
 }
 
